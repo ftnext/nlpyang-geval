c956cc1766b46054465265622823b91f0d86229c
ftnext <takuyafjp+develop@gmail.com>
2025-11-03 15:26:52 +0900

    Run with openai v1

--- gpt4_eval.py
+++ gpt4_eval.py
@@ -1,8 +1,16 @@
-import openai
-import json
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "openai",
+#     "tqdm",
+# ]
+# ///
 import argparse
-import tqdm
+import json
 import time
+
+import tqdm
+from openai import OpenAI
 
 if __name__ == '__main__':
 
@@ -10,10 +18,10 @@ if __name__ == '__main__':
     argparser.add_argument('--prompt_fp', type=str, default='prompts\summeval\con_detailed.txt')
     argparser.add_argument('--save_fp', type=str, default='results\gpt4_con_detailed_openai.json')
     argparser.add_argument('--summeval_fp', type=str, default='data\summeval.json')
-    argparser.add_argument('--key', type=str, required=True)
     argparser.add_argument('--model', type=str, default='gpt-4-0613')
     args = argparser.parse_args()
-    openai.api_key = args.key
+
+    client = OpenAI()  # env var: OPENAI_API_KEY
 
     summeval = json.load(open(args.summeval_fp))
     prompt = open(args.prompt_fp).read()
@@ -28,11 +36,11 @@ if __name__ == '__main__':
         instance['prompt'] = cur_prompt
         while True:
             try:
-                _response = openai.ChatCompletion.create(
+                _response = client.chat.completions.create(
                     model=args.model,
                     messages=[{"role": "system", "content": cur_prompt}],
-                    temperature=2,
-                    max_tokens=5,
+                    temperature=1,  # ref: 2303.16634v3 3.1
+                    max_tokens=6,  # Support "- Coherence: 3" (6 tokens)
                     top_p=1,
                     frequency_penalty=0,
                     presence_penalty=0,
@@ -42,8 +50,8 @@ if __name__ == '__main__':
                 )
                 time.sleep(0.5)
 
-                all_responses = [_response['choices'][i]['message']['content'] for i in
-                                 range(len(_response['choices']))]
+                all_responses = [_response.choices[i].message.content for i in
+                                 range(len(_response.choices))]
                 instance['all_responses'] = all_responses
                 new_json.append(instance)
                 ct += 1
